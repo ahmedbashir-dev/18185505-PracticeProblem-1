ae2184b3fbb9b0284b80e6be9e71e5557298b9d6
= <=>
2020-08-07 15:59:05 +0500

    Added Rectangle class

---
+++ src/com/abstraction/Rectangle.java
@@ -0,0 +1,27 @@
+package com.abstraction;
+
+public class Rectangle extends Shape{
+    private double length,width;
+
+    public Rectangle(String color, double length, double width) {
+        super(color);
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+}
